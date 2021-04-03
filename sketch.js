9b46fb0764d5071c5df488a0b3f7d98c37caff17
InfernoChiken <81037231+InfernoChiken@users.noreply.github.com>
2021-04-03 11:53:34 -0500

    bounceOff + library

--- sketch.js
+++ sketch.js
@@ -1,31 +1,69 @@
 var moving, fixed;
 
+var rect1, rect2, rect3, rect4;
+
 function setup() {
   createCanvas(600, 600);
-  moving = createSprite(300, 300, 40, 80);
-  fixed = createSprite(300, 300, 60, 60);
+  moving = createSprite(10, 300, 40, 80);
+  fixed = createSprite(300, 300, 50, 50);
+
+  rect1 = createSprite(150, 150, 50, 50);
+  rect2 = createSprite(250, 150, 50, 50);
+  rect3 = createSprite(350, 150, 50, 50);
+  rect4 = createSprite(450, 150, 50, 50);
 
   moving.shapeColor = "yellow";
   fixed.shapeColor = "green";
+
+  moving.velocityX = 3;
 }
 
 function draw() {
-
   background(0);
 
-  moving.x = mouseX;
-  moving.y = mouseY;
-
-  if (moving.x - fixed.x < moving.width / 2 + fixed.width / 2 &&
-    fixed.x - moving.x < moving.width/2 + fixed.width/2 &&
-    moving.y - fixed.y < moving.height/2 + fixed.height/2 &&
-    fixed.y - moving.y < moving.height/2 + fixed.height/2) {
+  if (isTouching(moving, fixed)) {
     moving.shapeColor = "red";
     fixed.shapeColor = "blue";
   } else {
     moving.shapeColor = "yellow";
     fixed.shapeColor = "green";
+
+    if (isTouching(moving, rect4)) {
+      moving.shapeColor = "red";
+      rect4.shapeColor = "blue";
+    } else {
+      moving.shapeColor = "yellow";
+      rect4.shapeColor = "green";
+
+      if (isTouching(moving, rect1)) {
+        moving.shapeColor = "red";
+        rect1.shapeColor = "blue";
+      } else {
+        moving.shapeColor = "yellow";
+        rect1.shapeColor = "green";
+
+        if (isTouching(moving, rect2)) {
+          moving.shapeColor = "red";
+          rect2.shapeColor = "blue";
+        } else {
+          moving.shapeColor = "yellow";
+          rect2.shapeColor = "green";
+
+          if (isTouching(moving, rect3)) {
+            moving.shapeColor = "red";
+            rect3.shapeColor = "blue";
+          } else {
+            moving.shapeColor = "yellow";
+            rect3.shapeColor = "green";
+          }
+        }
+      }
+    }
   }
+
+  bounceOff(moving, fixed);
 
   drawSprites();
 }
+
+// defining function: parameters
